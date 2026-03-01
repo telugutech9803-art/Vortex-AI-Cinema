1b549a19244d9f8e77e10aada154b8c01094248b
telugutech9803-art <telugutech9803@gmail.com>
2026-03-01 18:50:32 +0530

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,8 @@
+import { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig = {
+  appId: 'com.vortex.aicinema',
+  appName: 'Vortex AI Cinema',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+export default config;
